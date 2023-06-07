1c0658dee3509607b8ba9c036cc0e2ab32340f2a
bobyseptiananda12 <bobymunthe48@gmail.com>
2023-06-08 03:25:07 +0700

    second commit

--- script/script.js
+++ script/script.js
@@ -5,49 +5,19 @@ let typed = new Typed('.ketik', {
     loop: true
 })
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('#nav').addClass('nav-active');
-        } else {
-            $('#nav').removeClass('nav-active');
-        }
+var sliderIndex = 0;
+var sliderTrack = document.querySelector('.slider-track');
+var slides = document.querySelectorAll('.slider-track img');
+var slideWidth = slides[0].clientWidth;
 
-        if ($(this).scrollTop() > 300) {
-            $('.scroll-up').addClass('show');
-        } else {
-            $('.scroll-up').removeClass('show');
+function changeSlide(n) {
+    sliderIndex += n;
 
-        }
-    });
+    if (sliderIndex < 0) {
+        sliderIndex = slides.length - 1;
+    } else if (sliderIndex >= slides.length) {
+        sliderIndex = 0;
+    }
 
-    // scroll offset
-    $('.page-scroll').on('click', function (e) {
-
-        let href = $(this).attr('href');
-
-        let tujuan = $(href);
-    })
-
-    // scroll button up
-    $('.scroll-up').on('click', function () {
-        $('html').animate({ scrollTop: 0 }, 1000)
-    })
-
-    // Nav items show -----
-
-    $('.menu-btn').on('click', function () {
-        $('.nav-text').toggleClass('active');
-        $('.menu-btn').toggleClass('active');
-
-    });
-
-    // -----
-
-    $('.nav-text ul li .page-scroll').on('click', function () {
-        $('.page-scroll').removeClass('active');
-
-        $(this).closest('li a').addClass('active')
-    })
-
-})
+    sliderTrack.style.transform = `translateX(-${sliderIndex * slideWidth}px)`;
+}

9d72974abf8222696f7b136cbc154f99155b77e8
bobyseptiananda12 <bobymunthe48@gmail.com>
2023-06-08 11:10:21 +0700

    third commit

--- script/script.js
+++ script/script.js
@@ -4,20 +4,3 @@ let typed = new Typed('.ketik', {
     backSpeed: 70,
     loop: true
 })
-
-var sliderIndex = 0;
-var sliderTrack = document.querySelector('.slider-track');
-var slides = document.querySelectorAll('.slider-track img');
-var slideWidth = slides[0].clientWidth;
-
-function changeSlide(n) {
-    sliderIndex += n;
-
-    if (sliderIndex < 0) {
-        sliderIndex = slides.length - 1;
-    } else if (sliderIndex >= slides.length) {
-        sliderIndex = 0;
-    }
-
-    sliderTrack.style.transform = `translateX(-${sliderIndex * slideWidth}px)`;
-}
